3fad62fb7a5ab7b6d86f8db974ed9b70bd710995
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-21 11:22:01 -0300

    Criando a rota de detalhes de usuario

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import { CreateUserController } from '../controllers/users/CreateUserController'
 import { AuthUserController } from '../controllers/users/AuthUserController'
+import { DetailUserController } from '../controllers/users/DetailUserController'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', async (req: FastifyRequest, reply: FastifyReply) => {
@@ -9,5 +10,9 @@ export async function appRoutes(app: FastifyInstance) {
 
   app.post('/users', async (req: FastifyRequest, reply: FastifyReply) => {
     return new AuthUserController().handle(req, reply)
+  })
+
+  app.get('/me', async (req: FastifyRequest, reply: FastifyReply) => {
+    return new DetailUserController().handle(req, reply)
   })
 }
