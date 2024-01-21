fae5ae66f4d9c7fedd0e37bb5fcf1b619a54faf0
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-21 11:36:26 -0300

    Adicionando fastifyJWT

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,29 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
 import { appRoutes } from './routes/routes'
+import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify({
   logger: true,
 })
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
--- backend/src/controllers/users/DetailUserController.ts
+++ backend/src/controllers/users/DetailUserController.ts
@@ -1,18 +1,20 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import {
-  DetailsUserUseCaseRequest,
-  DetailsUserUseCase,
-} from '../../use-cases/users/DetailUserUseCase'
+import { DetailsUserUseCase } from '../../use-cases/users/DetailUserUseCase'
 
 export class DetailUserController {
   async handle(req: FastifyRequest, reply: FastifyReply) {
     try {
-      const { id } = req.body as DetailsUserUseCaseRequest
-
       const detailsUserUseCase = new DetailsUserUseCase()
 
-      await detailsUserUseCase.execute({
-        id,
+      const { user } = await detailsUserUseCase.execute({
+        id: req.user.sub,
+      })
+
+      return reply.status(200).send({
+        user: {
+          ...user,
+          password: undefined,
+        },
       })
     } catch (err) {
       console.log(err)
