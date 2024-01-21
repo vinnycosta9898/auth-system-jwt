171213221141bba5a46bf0bca6a03730be00dbcd
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-21 01:14:04 -0300

    Criando a rota de autenticar usuario

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import { CreateUserController } from '../controllers/users/CreateUserController'
+import { AuthUserController } from '../controllers/users/AuthUserController'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', async (req: FastifyRequest, reply: FastifyReply) => {
     return new CreateUserController().handle(req, reply)
+  })
+
+  app.post('/users', async (req: FastifyRequest, reply: FastifyReply) => {
+    return new AuthUserController().handle(req, reply)
   })
 }
